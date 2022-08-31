34a969d4a0fa37b951aa39628c0044464ed7acd9
Ernesto Freyre <ernestofreyreg@gmail.com>
2022-08-30 21:28:11 -0700

    removed test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2 text-red-200 bg-blue-50'>
+    <div className='flex min-h-screen flex-col items-center justify-center bg-blue-50 py-2 text-red-200'>
       Hello
     </div>
   )
