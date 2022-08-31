6e34b4b882f68dd075721de5c4fc062fef9cd94a
Ernesto Freyre <ernestofreyreg@gmail.com>
2022-08-30 20:41:40 -0700

    initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+      Hello
+    </div>
+  )
+}
+
+export default Home
