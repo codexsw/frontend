1188a55c9664ab3eb3af3ef160f8f614b5ede88c
Ernesto Freyre <ernestofreyreg@gmail.com>
2022-08-30 20:56:21 -0700

    added plugins

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import type { NextPage } from 'next'
+import { useCallback, useState } from 'react'
 
 const Home: NextPage = () => {
+  const [count, setCount] = useState(0)
+
+  const handleClick = useCallback(() => {
+    setCount(count + 1)
+  }, [count])
+
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
-      Hello
+    <div className='flex min-h-screen flex-col items-center justify-center py-2 text-red-200 bg-blue-50'>
+      Hello {count}
+      <button onClick={handleClick}>+1</button>
     </div>
   )
 }
